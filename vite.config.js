41d9329161434132a89adf2f53265caa49e6b234
LucasHRC <154086546+LucasHRC@users.noreply.github.com>
2025-12-18 09:47:27 +0100

    init for pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/Rendu-3-AICG/',
   server: {
     port: 5173,
     host: 'localhost'
+  },
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets'
   }
 });
